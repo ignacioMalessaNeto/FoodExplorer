18ca8f985f0834b75f843b19c6487ee16c2a8dbb
Ignacio <projetosignacio@gmail.com>
2023-06-26 23:52:09 -0300

    application  structure button and footer component

---
+++ src/Components/Button/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Container = styled.button`
+    width: 106px;
+    color: ${({ theme }) => theme.COLORS.LIGHT_300};
+    font-family: 'Poppins', sans-serif;
+    font-weight: 300;
+`;
---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import  GlobalStyles from './Styles/global';
+import { ThemeProvider } from 'styled-components';
+import theme from './Styles/theme'
+
+import { Details } from './Pages/Details';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+    <GlobalStyles />
+    <Details/>
+    </ThemeProvider>
+  </React.StrictMode>,
+)
